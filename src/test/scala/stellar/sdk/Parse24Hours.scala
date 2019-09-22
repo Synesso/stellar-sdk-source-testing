6f6feff74e0979ae1b71d164c2675224ff5f1a68
Jem Mawson <jmawson@squareup.com>
2019-09-22 22:21:31 +1000

    Parse 1 days worth of txns

--- src/test/scala/stellar/sdk/Parse24Hours.scala
+++ src/test/scala/stellar/sdk/Parse24Hours.scala
@@ -4,22 +4,26 @@ import java.time.ZonedDateTime
 
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
+import stellar.sdk.model.ledger.TransactionLedgerEntries
 import stellar.sdk.model.{Desc, Now}
 
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.{Success, Try}
 
 class Parse24Hours(implicit val ee: ExecutionEnv) extends Specification {
 
   "streaming transactions" should {
     "be successful" >> {
-      val oneDayAgo = ZonedDateTime.now().minusHours(1)
-      Await.result(PublicNetwork.transactions(Now, Desc).map { stream =>
+      val oneDayAgo = ZonedDateTime.now().minusDays(1)
+      PublicNetwork.transactions(Now, Desc).map { stream =>
         stream
           .takeWhile(_.createdAt.isAfter(oneDayAgo))
-          .map { th => print('.'); th.ledgerEntries }
-          .toList
-      }, 10.minutes) must not(beEmpty)
+          .map { th => Try(th.ledgerEntries) }
+          .foldLeft(List.empty[Try[TransactionLedgerEntries]]) {
+            case (acc, Success(t)) => acc
+            case (acc, failure) => failure +: acc
+          }
+      } must beEmpty[List[Try[TransactionLedgerEntries]]].awaitFor(10.minutes)
     }
   }
 
