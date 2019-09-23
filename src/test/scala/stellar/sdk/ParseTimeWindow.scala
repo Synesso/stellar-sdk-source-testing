3482b88b7112cf622e5cff5b2146175002206d47
Jem Mawson <jmawson@squareup.com>
2019-09-23 21:29:26 +1000

    Stream an hour of ledger responses

--- src/test/scala/stellar/sdk/ParseTimeWindow.scala
+++ src/test/scala/stellar/sdk/ParseTimeWindow.scala
@@ -5,6 +5,7 @@ import java.time.ZonedDateTime
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
 import stellar.sdk.model.ledger.TransactionLedgerEntries
+import stellar.sdk.model.response.LedgerResponse
 import stellar.sdk.model.{Desc, Now}
 
 import scala.concurrent.duration._
@@ -24,6 +25,17 @@ class ParseTimeWindow(implicit val ee: ExecutionEnv) extends Specification {
             case (acc, failure) => failure +: acc
           }
       } must beEmpty[List[Try[TransactionLedgerEntries]]].awaitFor(10.minutes)
+    }
+  }
+
+  "streaming ledgers" should {
+    "be successful" >> {
+      val timeWindow = ZonedDateTime.now().minusHours(1)
+      PublicNetwork.ledgers(Now, Desc).map { stream =>
+        stream
+          .takeWhile(_.closedAt.isAfter(timeWindow))
+          .reduceLeft[LedgerResponse] { case (_, next) => next }
+      } must not(beNull[LedgerResponse]).awaitFor(10.minutes)
     }
   }
 
