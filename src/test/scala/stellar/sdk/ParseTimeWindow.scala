c7737fd03da479eeebeb49dbd5064fe514d29e98
Jem Mawson <jmawson@squareup.com>
2020-03-09 18:48:24 +1000

    Logs the quantity of entities parsed

--- src/test/scala/stellar/sdk/ParseTimeWindow.scala
+++ src/test/scala/stellar/sdk/ParseTimeWindow.scala
@@ -2,40 +2,48 @@ package stellar.sdk
 
 import java.time.ZonedDateTime
 
+import com.typesafe.scalalogging.LazyLogging
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.mutable.Specification
 import stellar.sdk.model.ledger.TransactionLedgerEntries
 import stellar.sdk.model.response.LedgerResponse
 import stellar.sdk.model.{Desc, Now}
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.util.{Success, Try}
 
-class ParseTimeWindow(implicit val ee: ExecutionEnv) extends Specification {
+class ParseTimeWindow(implicit val ee: ExecutionEnv) extends Specification with LazyLogging {
 
   "streaming transactions" should {
     "be successful" >> {
       val timeWindow = ZonedDateTime.now().minusHours(1)
-      PublicNetwork.transactions(Now, Desc).map { stream =>
+      val (failures, successCount) = Await.result(
+        PublicNetwork.transactions(Now, Desc).map { stream =>
         stream
           .takeWhile(_.createdAt.isAfter(timeWindow))
           .map { th => Try(th.ledgerEntries) }
-          .foldLeft(List.empty[Try[TransactionLedgerEntries]]) {
-            case (acc, Success(t)) => acc
-            case (acc, failure) => failure +: acc
+          .foldLeft((List.empty[Try[TransactionLedgerEntries]], 0)) {
+            case ((failures, successCount), Success(_)) => (failures, successCount + 1)
+            case ((failures, successCount), failure) => (failure +: failures, successCount)
           }
-      } must beEmpty[List[Try[TransactionLedgerEntries]]].awaitFor(10.minutes)
+      }, 10.minutes)
+      logger.info(s"Processed $successCount transactions")
+      failures must beEmpty[List[Try[TransactionLedgerEntries]]]
     }
   }
 
   "streaming ledgers" should {
     "be successful" >> {
       val timeWindow = ZonedDateTime.now().minusHours(1)
-      PublicNetwork.ledgers(Now, Desc).map { stream =>
+      val (ops, ledgers) = Await.result(PublicNetwork.ledgers(Now, Desc).map { stream =>
         stream
           .takeWhile(_.closedAt.isAfter(timeWindow))
-          .reduceLeft[LedgerResponse] { case (_, next) => next }
-      } must not(beNull[LedgerResponse]).awaitFor(10.minutes)
+          .foldLeft((0, 0)) { case ((opCount, ledgerCount), next) => (opCount + next.operationCount, ledgerCount + 1) }
+      }, 10.minutes)
+      logger.info(s"Processed $ledgers ledgers, with $ops operations.")
+      ledgers must beGreaterThan(0)
+      ops must beGreaterThan(0)
     }
   }
 
